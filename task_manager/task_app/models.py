62127006bf8981bdb2c8bab99bf3b8d396e335bb
onelonelyowl <64985946+onelonelyowl@users.noreply.github.com>
2023-11-30 09:49:53 +0000

    task model created

--- task_manager/task_app/models.py
+++ task_manager/task_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField("Task name", max_length=200)
+    description = models.CharField("Task description", max_length=500)
+    due_date = models.DateTimeField("Due date")
+    is_completed = models.BooleanField("Is completed")
