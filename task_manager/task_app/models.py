36d4cd3f5a50a591fbe092e4e1a5bd748216f8be
onelonelyowl <64985946+onelonelyowl@users.noreply.github.com>
2023-11-30 10:17:32 +0000

    added created_by

--- task_manager/task_app/models.py
+++ task_manager/task_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 class Task(models.Model):
     def __str__(self):
@@ -7,6 +8,7 @@ class Task(models.Model):
     name = models.CharField("Task name", max_length=200)
     description = models.CharField("Task description", max_length=500)
     due_date = models.DateTimeField("Due date")
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
     is_completed = models.BooleanField("Is completed")
     def is_overdue(self):
         return self.due_date <= timezone.now()
