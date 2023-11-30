ac3a6d4aca72e45d19685303e391e2d3e2702f9b
onelonelyowl <64985946+onelonelyowl@users.noreply.github.com>
2023-11-30 10:02:23 +0000

    update task model

--- task_manager/task_app/models.py
+++ task_manager/task_app/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 class Task(models.Model):
+    def __str__(self):
+        return self.name
     name = models.CharField("Task name", max_length=200)
     description = models.CharField("Task description", max_length=500)
     due_date = models.DateTimeField("Due date")
     is_completed = models.BooleanField("Is completed")
+    def is_overdue(self):
+        return self.due_date <= timezone.now()
