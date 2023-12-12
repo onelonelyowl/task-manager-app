13afecefe5180fb855fa36912d64c7221b24bd95
onelonelyowl <64985946+onelonelyowl@users.noreply.github.com>
2023-12-12 16:18:59 +0000

    empty url now redirects to tasks

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', lambda request: redirect('tasks/', permanent=False)),
     path('tasks/', include('task_app.urls')),
+    path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
     path("accounts/", include('django.contrib.auth.urls')),
 ]
